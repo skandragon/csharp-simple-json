8ca14dc3b402df1d8141b416a0bf0da777d7d425
Michael Graff <explorer@flame.org>
2021-02-21 03:39:50 -0600

    code style, and support fields

--- json.cs
+++ json.cs
@@ -6,7 +6,8 @@ namespace org.flame.SimpleJson {
     public class MainClass {
             public static void Main(string[] args) {
                 var x = new Example();
-                x.Field1 = "string1";
+                x.Property1 = "string1";
+                x.field1 = "field1string";
                 x.Address = new PostalAddressExample("main st.", 12345);
 
                 Console.WriteLine(SkandragonSimpleJson.ToJson(x));
@@ -25,7 +26,9 @@ namespace org.flame.SimpleJson {
             }
     }
 
-    public class SkandragonJsonName: Attribute {
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class SkandragonJsonName: Attribute
+    {
         public string Value {get; }
 
         public SkandragonJsonName(string s) {
@@ -33,11 +36,14 @@ namespace org.flame.SimpleJson {
         }
     }
 
-    public class SkandragonJsonIgnore: Attribute {
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class SkandragonJsonIgnore: Attribute
+    {
         public SkandragonJsonIgnore() {}
     }
 
-    public class PostalAddressExample {
+    public class PostalAddressExample
+    {
         public string Street {get; set;}
         public int ZipCode {get; set;}
 
@@ -47,9 +53,10 @@ namespace org.flame.SimpleJson {
         }
     }
 
-    public class Example {
-        [SkandragonJsonName("field1")]
-        public string Field1 {get; set; }
+    public class Example
+    {
+        [SkandragonJsonName("prop1")]
+        public string Property1 {get; set; }
 
         public int IntField {get; set; }
 
@@ -61,49 +68,81 @@ namespace org.flame.SimpleJson {
         private int PrivateIntField {get; set;}
 
         public PostalAddressExample Address {get; set;}
+
+        public string field1;
     }
 
     public class SkandragonSimpleJson {
-        private static string GetJsonName(PropertyInfo prop) {
+        private static string GetJsonName(PropertyInfo prop)
+        {
             var a = (SkandragonJsonName)Attribute.GetCustomAttribute(prop, typeof(SkandragonJsonName));
-            if (a == null) {
+            if (a == null)
+            {
                 return prop.Name;
             }
             return a.Value;
         }
 
-        private static bool ShouldSkip(PropertyInfo prop) {
+        private static string GetJsonName(FieldInfo f)
+        {
+            var a = (SkandragonJsonName)Attribute.GetCustomAttribute(f, typeof(SkandragonJsonName));
+            if (a == null)
+            {
+                return f.Name;
+            }
+            return a.Value;
+        }
+
+        private static bool ShouldSkip(PropertyInfo prop)
+        {
             var a = (SkandragonJsonIgnore)Attribute.GetCustomAttribute(prop, typeof(SkandragonJsonIgnore));
             return a != null;
         }
 
-        private static string EncodeString(string o) {
+        private static bool ShouldSkip(FieldInfo f)
+        {
+            var a = (SkandragonJsonIgnore)Attribute.GetCustomAttribute(f, typeof(SkandragonJsonIgnore));
+            return a != null;
+        }
+
+        private static string EncodeString(string o)
+        {
             var sb = new StringBuilder("\"", o.Length + 2);
-            foreach (char c in o) {
+            foreach (char c in o)
+            {
                 sb.Append(RawEncodeChar(c));
             }
             sb.Append("\"");
             return sb.ToString();
         }
 
-        private static string RawEncodeChar(char c) {
-            if (c == '\\') {
+        private static string RawEncodeChar(char c)
+        {
+            if (c == '\\')
+            {
                 return "\\\\";
-            } else if (c == '"') {
+            }
+            else if (c == '"')
+            {
                 return "\\\"";
-            } else if (c < 0x001f) {
+            }
+            else if (c < 0x001f)
+            {
                 uint x = c;
                 return "\\u" + x.ToString("x04"); // TODO: handle > 2 byte characters
             }
             return c.ToString();
         }
 
-        private static string EncodeChar(char c) {
+        private static string EncodeChar(char c)
+        {
             return "\"" + RawEncodeChar(c) + "\"";
         }
 
-        public static string ToJson(object o) {
-            if (o == null) {
+        public static string ToJson(object o)
+        {
+            if (o == null)
+            {
                 return "null";
             }
 
@@ -113,31 +152,58 @@ namespace org.flame.SimpleJson {
                 || ty == typeof(long) || ty == typeof(ulong)
                 || ty == typeof(float) || ty == typeof(double) || ty == typeof(decimal)
                 || ty == typeof(short) || ty == typeof(ushort)
-                || ty == typeof(byte) || ty == typeof(sbyte)
-                ) {
+                || ty == typeof(byte) || ty == typeof(sbyte))
+            {
                 return o.ToString();
-            } else if (ty == typeof(string)) {
+            }
+            else if (ty == typeof(string))
+            {
                 return EncodeString((string)o);
-            } else if (ty == typeof(char)) {
+            }
+            else if (ty == typeof(char))
+            {
                 return EncodeChar((char)o);
-            } else if (ty == typeof(bool)) {
+            }
+            else if (ty == typeof(bool))
+            {
                 return (bool)o ? "true" : "false";
             }
 
             var sb = new StringBuilder("{");
-            var props = o.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
             var first = true;
-            foreach (PropertyInfo prop in props) {
-                if (ShouldSkip(prop)) {
+
+            var props = o.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in props)
+            {
+                if (ShouldSkip(prop))
+                {
                     continue;
                 }
-                if (!first) {
+                if (!first)
+                {
                     sb.Append(",");
                 }
                 first = false;
                 sb.Append(EncodeString(GetJsonName(prop)) + ":");
                 sb.Append(ToJson(prop.GetValue(o, null)));
             }
+
+            var fields = o.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+            foreach (FieldInfo field in fields)
+            {
+                if (ShouldSkip(field))
+                {
+                    continue;
+                }
+                if (!first)
+                {
+                    sb.Append(",");
+                }
+                first = false;
+                sb.Append(EncodeString(GetJsonName(field)) + ":");
+                sb.Append(ToJson(field.GetValue(o)));
+            }
+
             sb.Append("}");
             return sb.ToString();
         }
